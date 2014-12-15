03097dc5d3013d2a20e4b42862ead75cbcb016c9
Jason Damiani <me@jasondamiani.com>
2014-12-15 15:15:55 -0500

    Completed problem 2.2

--- Lesson_2_Problem_Set/02-Airport_List/airports.py
+++ Lesson_2_Problem_Set/02-Airport_List/airports.py
@@ -2,16 +2,22 @@
 # -*- coding: utf-8 -*-
 # All your changes should be in the 'extract_airports' function
 # It should return a list of airport codes, excluding any combinations like "All"
-
+import string
 from bs4 import BeautifulSoup
 html_page = "options.html"
 
 
 def extract_airports(page):
     data = []
+
     with open(page, "r") as html:
-        # do something here to find the necessary values
-        soup = BeautifulSoup(html)
+      # do something here to find the necessary values
+      soup = BeautifulSoup(html)
+
+      for airport in soup.find(id="AirportList").find_all("option"):
+
+        if string.find(airport['value'].lower(), 'all') != 0:
+          data.append(airport['value'])
 
     return data
 
